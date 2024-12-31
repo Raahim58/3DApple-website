a220cd8dd0e11eef28b2b19b8e197969f2bcd7c0
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-01 00:12:51 +0500

    created highlights section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useEffect, useState } from 'react';
+import { heroVideo, smallHeroVideo } from '../utils';
 
-const hero = () => {
-  return (
-    <div>
-        Hero
-    </div>
-  )
-}
+const Hero = () => {
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo)
 
-export default hero
+    const handleVideoSrcSet = () => {
+        if(window.innerWidth < 768) {
+            setVideoSrc(smallHeroVideo)
+        } else {
+            setVideoSrc(heroVideo)
+        }
+    }
+
+    // Update video source on window resize
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoSrcSet)
+        return () => window.removeEventListener('resize', handleVideoSrcSet)
+    }, []);
+
+    // GSAP animation
+    useEffect(() => {
+        gsap.to('#hero', { opacity: 1, delay: 1.5 })
+        gsap.to('#cta', { opacity: 1, y: -50, delay: 2.5 })
+    }, [])
+
+    return (
+        <section className="w-full nav-height bg-black relative">
+            <div className="h-5/6 w-full flex-center flex-col">
+                <p id="hero" className="hero-title">iPhone 15 Pro</p>
+                <div className="md:w-10/12 w-9/12">
+                <video className="pointer-events-none"
+                    autoPlay 
+                    muted 
+                    playsInline >
+                    <source src={videoSrc} type="video/mp4" />
+                </video>
+                </div>
+            </div>
+
+            <div
+                id="cta"
+                className="flex flex-col items-center opacity-0 translate-y-20"
+            >
+                <a href="#highlights" className="btn">Buy</a>
+                <p className="font-normal text-xl">From $199/month or $999</p>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
