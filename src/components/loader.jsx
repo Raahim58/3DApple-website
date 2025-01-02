ed184857c83ab4f0eb9fe36400ac382916225b37
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-02 19:42:26 +0500

    style loading effect

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Html } from '@react-three/drei'
 
-// add nice 2d or 3d loader
-const loader = () => {
+const Loader = () => {
     return (
         <Html>
             <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-                <div className='w-[10vw] h-[10vw] rounded-full'>
-                    Loading...
-                </div>
+                <div className='w-[10vw] h-[10vw] rounded-full border-4 border-t-4 border-blue-500 animate-spin'/>
             </div>
         </Html>
     )
 }
 
-export default loader
+export default Loader
